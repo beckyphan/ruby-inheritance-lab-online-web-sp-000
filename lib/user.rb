34bc11993730f74d6bae4b527d92201862110b7d
Becky Phan <beckyphan@live.com>
2019-06-12 00:12:03 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,15 +1,12 @@
 class User 
   attr_accessor :first_name, :last_name
   
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize
+    knowledge = []
   end
   
   def knowledge
     @knowledge
   end
-  
-  
   
 end 
